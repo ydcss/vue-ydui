009000b94cc2ccf7cd1fca99814cb17f0b02aab5
Ooo_My_God <surging2@qq.com>
2017-07-20 17:27:32 +0800

    修改 yd-cityselect 组件数据源将由用户提供，修改返回数据格式。

--- example/routers/cityselect.vue
+++ example/routers/cityselect.vue
@@ -4,40 +4,45 @@
         <yd-cell-group title="默认调用" class="demo-small-pitch">
             <yd-cell-item arrow>
                 <span slot="left">所在地区：</span>
-                <input slot="right" type="text" @click.stop="show1 = true" v-model="model1" readonly class="cell-input" placeholder="请选择收货地址">
+                <input slot="right" type="text" @click.stop="show1 = true" v-model="model1" readonly placeholder="请选择收货地址">
             </yd-cell-item>
         </yd-cell-group>
 
         <yd-cell-group title="设置默认值">
             <yd-cell-item arrow>
                 <span slot="left">所在地区：</span>
-                <input slot="right" type="text" @click.stop="show2 = true" v-model="model2" readonly class="cell-input" placeholder="请选择收货地址">
+                <input slot="right" type="text" @click.stop="show2 = true" v-model="model2" readonly placeholder="请选择收货地址">
             </yd-cell-item>
         </yd-cell-group>
 
-        <yd-cityselect v-model="show1" :done="result1"></yd-cityselect>
+        <yd-cityselect v-model="show1" :done="result1" :items="district"></yd-cityselect>
 
-        <yd-cityselect v-model="show2" :done="result2" provance="新疆" city="乌鲁木齐市" area="天山区"></yd-cityselect>
+        <yd-cityselect v-model="show2" :done="result2" :items="district" provance="新疆" city="乌鲁木齐市" area="天山区"></yd-cityselect>
 
     </yd-layout>
 </template>
 
 <script type="text/babel">
+    import District from 'ydui-district/dist/jd_province_city_area_id';
+
     export default {
         data() {
             return {
                 show1: false,
                 show2: false,
                 model1: '',
-                model2: '新疆 乌鲁木齐市 天山区'
+                model2: '新疆 乌鲁木齐市 天山区',
+                district: District
             }
         },
         methods: {
             result1(ret) {
-                this.model1 = ret.provance + ' ' + ret.city + ' ' + ret.area;
+                console.log(ret);
+                this.model1 = ret.itemName1 + ' ' + ret.itemName2 + ' ' + ret.itemName3;
             },
             result2(ret) {
-                this.model2 = ret.provance + ' ' + ret.city + ' ' + ret.area;
+                console.log(ret);
+                this.model2 = ret.itemName1 + ' ' + ret.itemName2 + ' ' + ret.itemName3;
             }
         }
     }
--- src/components/cityselect/src/cityselect.vue
+++ src/components/cityselect/src/cityselect.vue
@@ -3,49 +3,26 @@
         <div class="mask-cityselect" v-show="show" @click.stop="close"></div>
         <div class="m-cityselect" :class="show ? 'cityselect-active' : ''">
             <div class="cityselect-header">
-                <p class="cityselect-title">所在地区</p>
+                <p class="cityselect-title">{{title}}</p>
                 <div v-show="ready" class="cityselect-nav">
                     <a href="javascript:;"
-                       v-show="!!nav.txt1"
-                       @click.stop="backoffView(0)"
-                       :class="nav.index == 0 ? 'cityselect-nav-active' : ''">{{nav.txt1}}</a>
-                    <a href="javascript:;"
-                       v-show="!!nav.txt2"
-                       @click.stop="backoffView(1)"
-                       :class="nav.index == 1 ? 'cityselect-nav-active' : ''">{{nav.txt2}}</a>
-                    <a href="javascript:;"
-                       v-show="!!nav.txt3"
-                       @click.stop="forwardView(true)"
-                       :class="nav.index == 2 ? 'cityselect-nav-active' : ''">{{nav.txt3}}</a>
+                       v-for="index in columnNum"
+                       v-show="!!nav['txt' + index]"
+                       @click.stop="navEvent(index)"
+                       :class="index == navIndex ? 'cityselect-nav-active' : ''"
+                    >{{nav['txt' + index]}}</a>
                 </div>
             </div>
             <div v-show="!ready" class="cityselect-loading">加载中</div>
             <ul v-show="ready" class="cityselect-content" :class="activeClasses">
-                <li class="cityselect-item" ref="provanceBox">
+                <li class="cityselect-item" v-for="index in columnNum" :ref="'itemBox' + index">
                     <div class="cityselect-item-box">
                         <a href="javascript:;"
-                           :class="item.n == activeProvance ? 'cityselect-item-active' : ''"
-                           v-for="item in citys"
-                           @click.stop="chooseProvance(item.n, item.c)"
+                           :data="item.v" :data2="active['itemValue' + index]"
+                           v-for="item in columns['columnItems' + index]"
+                           :class="currentClass(item.v, item.n, index)"
+                           @click.stop="itemEvent(index, item.n, item.v, item.c)"
                         ><span>{{item.n}}</span></a>
-                    </div>
-                </li>
-                <li class="cityselect-item" ref="cityBox">
-                    <div class="cityselect-item-box">
-                        <a href="javascript:;"
-                           :class="item.n == activeCity ? 'cityselect-item-active' : ''"
-                           v-for="item in cityArray"
-                           @click.stop="chooseCity(item.n, item.a)"
-                        ><span>{{item.n}}</span></a>
-                    </div>
-                </li>
-                <li class="cityselect-item" ref="areaBox">
-                    <div class="cityselect-item-box">
-                        <a href="javascript:;"
-                           :class="item == activeArea ? 'cityselect-item-active' : ''"
-                           v-for="item in areaArray"
-                           @click.stop="chooseArea(item)"
-                        ><span>{{item}}</span></a>
                     </div>
                 </li>
             </ul>
@@ -55,27 +32,27 @@
 
 <script type="text/babel">
     import {addClass, removeClass, getScrollview, isIOS} from '../../../utils/assist';
-    import Citys from './ydui.citys';
 
     export default {
         name: 'yd-cityselect',
         data() {
             return {
-                citys: Citys,
                 show: this.value,
+                navIndex: 1,
                 nav: {
-                    index: 0,
                     txt1: '请选择',
                     txt2: '',
                     txt3: ''
                 },
-                activeProvance: '',
-                activeCity: '',
-                activeArea: '',
-                cityArray: [],
-                areaArray: [],
+                columns: {
+                    columnItems1: this.items,
+                    columnItems2: [],
+                    columnItems3: []
+                },
+                active: {},
                 activeClasses: '',
-                itemHeight: 40
+                itemHeight: 40,
+                columnNum: 1
             }
         },
         props: {
@@ -87,9 +64,17 @@
             city: String,
             area: String,
             done: Function,
+            title: {
+                type: String,
+                default: '所在地区'
+            },
             value: {
                 type: Boolean,
                 default: false
+            },
+            items: {
+                type: Array,
+                required: true
             }
         },
         watch: {
@@ -110,94 +95,89 @@
         },
         methods: {
             init() {
+                this.scrollView = getScrollview(this.$el);
+
                 if (!this.ready)return;
 
-                this.setDefalutValue();
-
-                this.scrollView = getScrollview(this.$el);
+                this.isArray(this.items) && this.provance && this.setDefalutValue(this.items, 'provance', 1);
             },
-            chooseProvance(name, datas) {
-                this.nav.index = 1;
-                this.nav.txt1 = this.activeProvance = name;
-                this.nav.txt2 = '请选择';
-                this.nav.txt3 = '';
-                this.cityArray = datas;
-                this.activeCity = '';
-            },
-            chooseCity(name, datas) {
-                this.nav.txt2 = this.activeCity = name;
-                this.activeArea = '';
-                if (datas.length <= 0) {
-                    this.returnValue();
-                    return;
+            navEvent(index) {
+                if (this.columnNum > 2) {
+                    if (index >= this.columnNum) {
+                        this.forwardView(true);
+                    } else {
+                        this.backoffView();
+                    }
                 }
-                this.nav.txt3 = '请选择';
-                this.areaArray = datas;
-                this.forwardView(true);
+
+                this.navIndex = index;
             },
-            chooseArea(name) {
-                this.nav.txt3 = this.activeArea = name;
-                this.returnValue();
+            itemEvent(index, name, value, children) {
+                this.active['itemValue' + index] = value;
+                this.active['itemName' + index] = name;
+                this.nav['txt' + index] = name;
+                this.columns['columnItems' + (index + 1)] = children;
+
+                if (index > 1 && children && this.columnNum > 2) {
+                    this.forwardView(true);
+                }
+
+                this.clearNavTxt(index);
+
+                if (index === this.columnNum || children.length <= 0) {
+                    this.navIndex = index;
+                    this.returnValue();
+                } else {
+                    this.navIndex = index + 1;
+                    this.nav['txt' + (index + 1)] = '请选择';
+                }
             },
-            setDefalutValue() {
+            currentClass(v, n, index) {
+                return (v && v == this.active['itemValue' + index]) || (n && n === this.active['itemName' + index]) ? 'cityselect-item-active' : '';
+            },
+            clearNavTxt(index) {
+                for (let i = 0; i < this.columnNum; i++) {
+                    if (i > index) {
+                        this.nav['txt' + (i + 1)] = '';
+                    }
+                }
+            },
+            getColumsNum(arr) {
+                if (this.isArray(arr.c)) {
+                    this.columnNum++;
+                    this.getColumsNum(arr.c[0]);
+                }
+            },
+            isArray(arr) {
+                return arr && arr.constructor === Array && arr.length > 0;
+            },
+            setDefalutValue(items, currentValue, index) {
+                items.every((item, key) => {
+                    if (item.v == this[currentValue] || item.n === this[currentValue]) {
+                        const childrenItems = this.columns['columnItems' + (index + 1)] = item.c;
+                        const itemBox = this.$refs['itemBox' + index][0];
 
-                if (!this.provance)return;
+                        itemBox.scrollTop = key * this.itemHeight - itemBox.offsetHeight / 3;
 
-                this.citys.every((item1, index1) => {
+                        this.active['itemValue' + index] = item.v;
+                        this.active['itemName' + index] = item.n;
 
-                    if (item1.n == this.provance) {
+                        this.nav['txt' + index] = item.n;
+                        this.navIndex = index;
 
-                        const provanceBox = this.$refs.provanceBox;
-                        provanceBox.scrollTop = index1 * this.itemHeight - provanceBox.offsetHeight / 3;
+                        ++index;
 
-                        this.nav.index = 1;
-                        this.nav.txt1 = this.activeProvance = this.provance;
-                        this.nav.txt2 = '请选择';
+                        index >= this.columnNum && this.columnNum > 2 && this.forwardView(false);
 
-                        const _cityData = this.cityArray = item1.c;
+                        this.isArray(childrenItems) && this.setDefalutValue(childrenItems, ['', 'provance', 'city', 'area'][index], index);
 
-                        _cityData.every((item2, index2) => {
-
-                            if (item2.n == this.city) {
-
-                                const cityBox = this.$refs.cityBox;
-                                cityBox.scrollTop = index2 * this.itemHeight - cityBox.offsetHeight / 3;
-
-                                this.nav.txt2 = this.activeCity = this.city;
-
-                                const _areaData = this.areaArray = item2.a;
-
-                                const index3 = _areaData.indexOf(this.area);
-
-                                if (_areaData.length > 0) {
-                                    this.nav.index = 2;
-                                    this.nav.txt3 = '请选择';
-                                    this.forwardView(false);
-                                }
-
-                                if (index3 > -1) {
-
-                                    const areaBox = this.$refs.areaBox;
-                                    areaBox.scrollTop = index3 * this.itemHeight - areaBox.offsetHeight / 3;
-
-                                    this.nav.txt3 = this.activeArea = this.area;
-                                }
-
-                                return false;
-                            }
-                            return true;
-                        });
                         return false;
                     }
                     return true;
                 });
             },
             returnValue() {
-                this.done({
-                    provance: this.activeProvance,
-                    city: this.activeCity,
-                    area: this.activeArea
-                });
+                this.done(this.active);
                 this.close();
             },
             close() {
@@ -206,14 +186,15 @@
                 this.$emit('input', false);
                 this.show = false;
             },
-            backoffView(index) {
-                this.nav.index = index;
+            backoffView() {
                 this.activeClasses = 'cityselect-move-animate cityselect-prev';
             },
             forwardView(animate) {
-                this.nav.index = 2;
                 this.activeClasses = (animate ? 'cityselect-move-animate' : '') + ' cityselect-next';
             }
+        },
+        created() {
+            this.items && this.items[0] && this.getColumsNum(this.items[0]);
         },
         mounted() {
             this.init();
