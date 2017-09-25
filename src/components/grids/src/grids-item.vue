e7e5d6559bc659331b38d071ece7e9168e492788
Ooo_My_God <surging2@qq.com>
2017-09-25 15:44:03 +0800

    修复 yd-grid-item 不能传入Object问题。

--- src/components/grids/src/grids-item.vue
+++ src/components/grids/src/grids-item.vue
@@ -14,7 +14,7 @@
     export default {
         name: 'yd-grids-item',
         props: {
-            link: String
+            link: [String, Object]
         },
         computed: {
             checkIcon() {
