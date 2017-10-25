d12951af355ea7fe47e989860cef2aa52d6688af
Ooo_My_God <surging2@qq.com>
2017-10-25 11:05:34 +0800

    组件 yd-tabbar-item 添加type和tag属性，方便实现普通链接跳转。

--- src/components/tabbar/src/tabbar-item.vue
+++ src/components/tabbar/src/tabbar-item.vue
@@ -1,6 +1,6 @@
 <template>
-    <router-link :to="link" class="yd-tabbar-item" :class="classes" :style="styles" :exact="$parent.exact"
-                 :active-class="$parent.activeClass">
+    <router-link v-if="type === 'link'" :to="link" class="yd-tabbar-item" :class="classes"
+            :style="styles" :exact="$parent.exact" :active-class="$parent.activeClass" :tag="tag">
         <span class="yd-tabbar-icon">
             <slot name="icon"></slot>
             <span class="yd-tabbar-badge">
@@ -10,6 +10,16 @@
         </span>
         <span class="yd-tabbar-txt">{{title}}</span>
     </router-link>
+    <a :href="link" class="yd-tabbar-item" :class="classes" :style="styles" v-else>
+        <span class="yd-tabbar-icon">
+            <slot name="icon"></slot>
+            <span class="yd-tabbar-badge">
+                <slot name="badge"></slot>
+            </span>
+            <span class="yd-tabbar-dot" v-if="dot"></span>
+        </span>
+        <span class="yd-tabbar-txt">{{title}}</span>
+    </a>
 </template>
 
 <script type="text/babel">
@@ -18,6 +28,13 @@
     export default {
         name: 'yd-tabbar-item',
         props: {
+            type: {
+                validator (value) {
+                    return ['link', 'a'].indexOf(value) > -1;
+                },
+                default: 'link'
+            },
+            tag: String,
             link: [String, Object],
             title: String,
             active: Boolean,
